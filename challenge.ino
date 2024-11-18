676be326a3679fc075acf0a39af30f1d94f2d4b2
EARTHISALWAYSHAPPY <107309420+EARTHISALWAYSHAPPY@users.noreply.github.com>
2024-11-18 18:10:48 +0700

    Update challenge.ino

--- challenge.ino
+++ challenge.ino
@@ -1,7 +1,7 @@
 // USE PWM pin(~) of Arduino UNO
-#define RedPin 9
+#define RedPin 11
 #define GreenPin 10
-#define BluePin 11
+#define BluePin 9
 #define Delaytime 100
 int Val_Red, Val_Green, Val_Blue;
 void setup()
