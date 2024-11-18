2ea53ba40baefa77f86437ceb49368354d72e4f5
EARTHISALWAYSHAPPY <107309420+EARTHISALWAYSHAPPY@users.noreply.github.com>
2024-11-19 01:00:58 +0700

    Update Problem2.ino

--- Problem2.ino
+++ Problem2.ino
@@ -1,42 +1,31 @@
 #include <DHT11.h>
-#define IRpin 2
 
 #define DHTPIN 4
 DHT11 dht11(DHTPIN);
-    
-#define RedPin 11
+
+#define RedPin 9
 #define GreenPin 10
-#define BluePin 9
-void setup()
-{
-  pinMode(IRpin, INPUT);
+#define BluePin 11
+void setup() {
+
   pinMode(RedPin, OUTPUT);
   pinMode(GreenPin, OUTPUT);
   pinMode(BluePin, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
   float temperature = dht11.readTemperature();
-  if (!digitalRead(IRpin))
-  {
-    if (temperature < 20)
-    {
-      digitalWrite(RedPin, 0);
-      digitalWrite(GreenPin, 0);
-      digitalWrite(BluePin, 255);
-    }
-    else if (temperature >= 20 && temperature <= 30)
-    {
-      digitalWrite(RedPin, 0);
-      digitalWrite(GreenPin, 255);
-      digitalWrite(BluePin, 0);
-    }
-    else if (temperature > 30)
-    {
-      digitalWrite(RedPin, 255);
-      digitalWrite(GreenPin, 0);
-      digitalWrite(BluePin, 0);
-    }
+  if (temperature < 20) {
+    digitalWrite(RedPin, 0);
+    digitalWrite(GreenPin, 0);
+    digitalWrite(BluePin, 255);
+  } else if (temperature >= 20 && temperature <= 30) {
+    digitalWrite(RedPin, 0);
+    digitalWrite(GreenPin, 255);
+    digitalWrite(BluePin, 0);
+  } else if (temperature > 30) {
+    digitalWrite(RedPin, 255);
+    digitalWrite(GreenPin, 0);
+    digitalWrite(BluePin, 0);
   }
 }
