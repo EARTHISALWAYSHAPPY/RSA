cd8d1a53bab734cc01bf503a2d9072f87ac51906
EARTHISALWAYSHAPPY <107309420+EARTHISALWAYSHAPPY@users.noreply.github.com>
2024-11-19 01:09:48 +0700

    Update Problem2.ino

--- Problem2.ino
+++ Problem2.ino
@@ -16,16 +16,16 @@ void setup() {
 void loop() {
   float temperature = dht11.readTemperature();
   if (temperature < 20) {
-    digitalWrite(RedPin, 0);
-    digitalWrite(GreenPin, 0);
-    digitalWrite(BluePin, 255);
-  } else if (temperature >= 20 && temperature <= 30) {
-    digitalWrite(RedPin, 0);
+    digitalWrite(RedPin, 255);
     digitalWrite(GreenPin, 255);
     digitalWrite(BluePin, 0);
-  } else if (temperature > 30) {
+  } else if (temperature >= 20 && temperature <= 30) {
     digitalWrite(RedPin, 255);
     digitalWrite(GreenPin, 0);
-    digitalWrite(BluePin, 0);
+    digitalWrite(BluePin, 255);
+  } else if (temperature > 30) {
+    digitalWrite(RedPin, 0);
+    digitalWrite(GreenPin, 255);
+    digitalWrite(BluePin, 255);
   }
 }
